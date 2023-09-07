762b079a84e5e797ae50fa0e866912c73767d82b
TimurDV88 <kumba-yo@yandex.ru>
2023-09-07 16:00:44 +0400

    fix: fixed "static" in StatEndPoints

--- stat-service/common/src/main/java/ru/practicum/ewm/StatEndPoints.java
+++ stat-service/common/src/main/java/ru/practicum/ewm/StatEndPoints.java
@@ -2,7 +2,7 @@ package ru.practicum.ewm;
 
 public class StatEndPoints {
 
-    public final static String POST_RECORD_PATH = "/hit";
-    public final static String GET_STAT_PATH = "/stats";
+    public static final String POST_RECORD_PATH = "/hit";
+    public static final String GET_STAT_PATH = "/stats";
 
 }
